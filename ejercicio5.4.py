c5bb19b21e930694e4c158dbba44465ca2ca1621
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-09-23 13:31:45 +0200

    Añadiendo informacion

--- ejercicio5.4.py
+++ ejercicio5.4.py
@@ -1,9 +1,11 @@
 from random import randint as rint
-def pedir_numero(prompt: str) -> int | None: # Al no poder importarse desde 2.5 lo hago así
+
+
+def pedir_numero(prompt: str) -> int | None:  # Al no poder importarse desde 2.5 lo hago así
     """
     La funcion pedira un numero y lo convertira a entero automaticamente
     Args:
-        prompt El prompt que se usara para pedir el numero
+        prompt: El prompt que se usara para pedir el numero
     Returns:
         El numero siendo un entero
     """
@@ -14,27 +16,37 @@ def pedir_numero(prompt: str) -> int | None: # Al no poder importarse desde 2.5 
             print(f"Introduce un valor valido, fallo: {e}")
             continue
 
+
 limite = 10
 
-numero_secreto = rint(1,limite)
+numero_secreto = rint(1, limite)
 
 hechos = 0
 intentos = 4
 
-def obtener_numero_mayor_menor(num_random,num_input):
-    if num_input>num_random:
+
+def obtener_numero_mayor_menor(num_random: int, num_input: int) -> str:
+    """
+    Obtener la representacion textual(en palabras) de si es mayor o menor
+    Args:
+        num_random: El numero aleatorio que quiero adivinar
+        num_input: El numero que proporcionas de input
+    Returns:
+        Mayor si el numero(num_input) es mayor que num_random, si num_input es menor que num_random devuelve menor
+    """
+    if num_input > num_random:
         return "mayor"
     else:
         return "menor"
 
 
-while hechos<intentos:
+while hechos < intentos:
     num = pedir_numero("Introduzca numero a adivinar: ")
     if num == numero_secreto:
         print("Enorabuena has ganado")
         break
-    elif num>limite or num<1:
+    elif num > limite or num < 1:
         print("El numero que ha introducido esta fuera de rango")
-    elif num!=numero_secreto:
+    elif num != numero_secreto:
         print(f"El numero {num} es {obtener_numero_mayor_menor(numero_secreto, num)}")
         hechos += 1
