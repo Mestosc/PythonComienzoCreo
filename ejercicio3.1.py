51af9b0c66bf9e05b18ef9b4d320a7eab5746ab8
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-09-25 08:43:43 +0200

    Añadiendo informacion

--- ejercicio3.1.py
+++ ejercicio3.1.py
@@ -1,7 +1,7 @@
 def obtener_numero_triangular(num: int):
     suma = 0
-    for i in range(1,num+1):
-        suma += i
+    for x in range(1, num + 1):
+        suma += x
     return suma
 
 for i in range(1,6+1):
