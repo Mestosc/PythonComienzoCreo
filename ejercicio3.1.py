d27b66062a6d0c76686645ecf169da34e90deb0b
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-09-29 12:28:36 +0200

    Añadiendo informacion

--- ejercicio3.1.py
+++ ejercicio3.1.py
@@ -1,4 +1,11 @@
-def obtener_numero_triangular(num: int):
+def obtener_numero_triangular(num: int) -> int:
+    """
+
+    Args:
+        num: El numero normal vamos a decir que queremos pasar a triangular
+    Returns:
+        El numero triangular
+    """
     suma = 0
     for x in range(1, num + 1):
         suma += x
